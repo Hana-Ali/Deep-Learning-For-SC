defd467d4a06585dda1020cd92007308e65815a6
Hana-Ali <hana142@live.com>
2023-08-08 23:06:58 +0000

    making number of streamlines bigger

--- Marmoset_Residual_Training/resnet_streamlines_test.py
+++ Marmoset_Residual_Training/resnet_streamlines_test.py
@@ -61,7 +61,7 @@ configs = {
     "streamline_arrays_path" : streamline_arrays_path, # Path to the streamlines array
     "batch_size" : 32, # Batch size
     "validation_batch_size" : 32, # Validation batch size
-    "num_streamlines" : 10, # Number of streamlines to consider from each site
+    "num_streamlines" : 50, # Number of streamlines to consider from each site
     
     ####### Parameters #######
     "initial_learning_rate" : 0.05, # Initial learning rate
