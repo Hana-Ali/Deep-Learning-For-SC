ee7d9930d4d15f5559d273ccadd90d61935bdfb1
Hana Ali <hana142@live.com>
2023-08-31 23:17:24 +0100

    fmri reg now works

--- Marmoset_Residual_Training/data_preprocessing/fmri_registration.py
+++ Marmoset_Residual_Training/data_preprocessing/fmri_registration.py
@@ -5,7 +5,7 @@ import nibabel as nib
 import subprocess
 
 # Function to register the fmri data
-def register_fmri(fmri_file, output_path, atlas_path):
+def register_fmri(fmri_file, output_path, atlas_path, transform_path):
 
     # Get the file name
     fmri_name = fmri_file.split(os.sep)[-1].split(".")[0]
@@ -48,13 +48,14 @@ def register_fmri(fmri_file, output_path, atlas_path):
 
         # Run the registration if the file doesn't exist
         if not os.path.exists(registration_filepath):
-            # Register the slice to the atlas using the flirt command
-            FLIRT_CMD = "flirt -in {fmri} -ref {atlas} -out {filepath} -omat {filepath}.mat".format(
-                fmri=slices_filepath_with_ext, atlas=atlas_path, filepath=registration_filepath
+            # Register the slice to the atlas using the ants command
+            ANTS_CMD = "antsApplyTransforms -i {fmri} -r {ref} -o {filepath}.nii.gz -t {transform} -n NearestNeighbor".format(
+                fmri=slices_filepath_with_ext, ref=atlas_path, filepath=registration_filepath, transform=transform_path
             )
+
             # Run the command
-            print("Running flirt command for slice {}".format(vol_idx))
-            subprocess.run(FLIRT_CMD, shell=True)
+            print("Running ANTS command for slice {}".format(vol_idx))
+            subprocess.run(ANTS_CMD, shell=True)
 
 
     # Grab all the slices in the registration
@@ -89,10 +90,12 @@ def main():
     hpc = int(sys.argv[1])
     if hpc:
         data_path = "/rds/general/ephemeral/user/hsa22/ephemeral/MBM_fmri/sub-NIHm32/ses-01"
-        atlas_path = "/rds/general/ephemeral/user/hsa22/ephemeral/Brain_MINDS/BMCR_STPT_template/Atlases/MBM_mapped/MBM_cortex_vPaxinos_80um_TC_std.nii.gz"
+        reference_path = "/rds/general/ephemeral/user/hsa22/ephemeral/Brain_MINDS/BMCR_STPT_template/Atlases/MBM_mapped/MBM_cortex_vM_80um_TC_std.nii.gz"
+        transform_path = "/rds/general/ephemeral/user/hsa22/ephemeral/MBM_fmri/MBM_2_STPT.h5"
     else:
         data_path = "/mnt/d/MBM_fmri/sub-NIHm32/ses-01"
-        atlas_path = "/mnt/d/Brain-MINDS/BMCR_STPT_template/Atlases/MBM_mapped/MBM_cortex_vPaxinos_80um_TC_std.nii.gz"
+        reference_path = "/mnt/d/Brain-MINDS/BMCR_STPT_template/Atlases/MBM_mapped/MBM_cortex_vM_80um_TC_std.nii.gz"
+        transform_path = "/mnt/d/MBM_fmri/MBM_2_STPT.h5"
 
     # Grab all the nii.gz files
     nii_gz_files = glob_files(data_path, "nii.gz")
@@ -109,10 +112,10 @@ def main():
     # Run the commands
     if hpc:
         file_idx = int(sys.argv[2])
-        register_fmri(nii_gz_files[file_idx], output_path, atlas_path)
+        register_fmri(nii_gz_files[file_idx], output_path, reference_path, transform_path)
     else:
         for file_idx in range(len(nii_gz_files)):
-            register_fmri(nii_gz_files[file_idx], output_path, atlas_path)
+            register_fmri(nii_gz_files[file_idx], output_path, reference_path, transform_path)
 
 
 if __name__ == "__main__":
